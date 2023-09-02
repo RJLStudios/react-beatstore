d9f71aed590bdeffcf6a3a537f04c45df379cfd9
Richard K <Richardkelly1612@hotmail.com>
2023-09-02 12:52:34 +0100

    Header styles

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return(
+    <div className='App'>
+    <div className='Navbar' /*ref={homeRef}*/>
+      <div className='Navbar-wrapper'>
+       <h3 className='Logo'>PRODLUNA</h3>
+       <ul className='Logo-links'>
+        <li className='Catalogue-btn' /*onClick={}*/></li>
+        </ul>
+      </div>
+      <ul className='Header-links'>
+        <li className='Catalogue-btn' /*onClick={}*/></li>
+        <li className='Pricing-btn' /*onClick={}*/></li>
+        <li className='Credits-btn' /*onClick={}*/></li>
+        <li className='Contact-btn' /*onClick={}*/></li>
+     </ul>
+    </div>
     </div>
   );
+     
+  
 }
 
 export default App;
